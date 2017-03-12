f6ef96991ac1d5790157eec3cf492e0d897e93f2
John Santos <johnsousasantos98@gmail.com>
2017-03-12 19:43:56 -0300

    Altera as funcionalidades e o tamanho da matriz

--- Ex1/Ex1Copia.c
+++ Ex1/Ex1Copia.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-void Funcao (int tab_int[100], char tab_char[100], int QtdeLeituras);
 
 int main(){
 	
 	int tab_int [100];
-	char tab_char[100];
+	char tab_char[50];
 	int Vezes = 0; 
 	
 	int QtdeLeituras = 0;
@@ -21,17 +20,5 @@ int main(){
 	 
 	scanf("%s", tab_char);
 	
-	Funcao(tab_int, tab_char, QtdeLeituras);
-
 }
 
-void Funcao (int tab_int[100], char tab_char[100], int QtdeLeituras){
-	int x = 0;
-	int y = 0;
-	
-	for(x=0; x < QtdeLeituras ; x++){
-		for(y=0; y < tab_int[x]; y++)
-			printf("%c", tab_char[x]);
-		printf("\n");
-	}		
-}

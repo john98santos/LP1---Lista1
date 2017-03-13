8e9c82000010cf8f2b352cb2ba84a772232a584b
John Santos <johnsousasantos98@gmail.com>
2017-03-13 01:02:33 -0300

    Alteração da função para funcionar com ímpares e pares

--- Ex2/Ex2.C
+++ Ex2/Ex2.C
@@ -1,37 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
+void funcao(char v[16]);
+
 int main(){
 
 	char  v[16];
 	int Meio;
-	int Meio2;
 	int Tamanho;
-	int x, y, z;
-	char txtMeio;
+	int inicial;
 	
 	printf("Informe alguns caracteres:");
 	scanf("%s", &v)	;
-		
+
+	funcao(v);	
+}
+void funcao(char v[16]){
+	int w, x, y, z;
+	int Meio;
+	int Tamanho;
+	int inicial;
+	
 	Tamanho = strlen(v);
-		 
-	//printf("%i\n", Tamanho); //imprimir tamanh da string
-		 
+	
 	if(Tamanho%2==0){
-		Meio =  (Tamanho/2)-1;
-		Meio2 = Tamanho/2;
-		printf("%c e %c", v[Meio], v[Meio2]);
+		inicial=1;
+		Meio =  (Tamanho/2);
+		
+		for(x=inicial; x<=Meio; x++){
+			for(y=(Meio-x); y < Meio; y++)
+				printf("%c", v[y]);
+			for(z=Meio; z < (Meio+x); z++)
+				printf("%c", v[z]);			
+			printf("\n");
+		}			
 	}else{
+		inicial=0;
 		Meio =  (Tamanho/2)+0.5;
-		Meio2 = Meio;		
-	}
 		
-	for(x=0; x<=Meio; x++){
-		for(y=(Meio-x); y < Meio; y++)
-			printf("%c", v[y]);
-		for(z=Meio; z <= (Meio+x); z++)
-			printf("%c", v[z]);
-		
-		printf("\n");
+		for(x=inicial; x<=Meio; x++){
+			for(y=(Meio-x); y < Meio; y++)
+				printf("%c", v[y]);
+			for(z=Meio; z <= (Meio+x); z++)
+				printf("%c", v[z]);			
+			printf("\n");
+		}	
 	}
+	
 }
